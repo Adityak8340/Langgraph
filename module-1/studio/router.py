fff710c89122cc8a19975267db893393f6e4a5cf
Aditya Tiwari <aditya.tiwari@data-smith.ai>
2025-05-16 11:41:22 +0530

    Langgraph with Gemini

---
+++ module-1/studio/router.py
@@ -0,0 +1,49 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+os.environ["LANGSMITH_ENDPOINT"] = os.getenv("LANGSMITH_ENDPOINT")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_PROJECT"] = os.getenv("LANGSMITH_PROJECT")
+
+
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# LLM with bound tool
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.1)
+llm_with_tools = llm.bind_tools([multiply])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
